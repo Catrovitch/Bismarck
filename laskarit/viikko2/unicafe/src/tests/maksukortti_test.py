c84253290cb60d4eb6e6f1a01cd43b7673e26972
alexander <ale.engelhardt@outlook.com>
2022-03-25 15:40:15 +0200

    update 2

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -5,5 +5,9 @@ class TestMaksukortti(unittest.TestCase):
     def setUp(self):
         self.maksukortti = Maksukortti(10)
 
+    def test_saldo_alussa(self):
+
+        self.assertNotEqual(str(self.maksukortti), "saldo: 10")
+
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
