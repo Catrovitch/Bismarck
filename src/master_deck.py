ffac21f0bb2627f5c373883f1efecd8668eebbba
alexander <ale.engelhardt@outlook.com>
2022-03-31 15:45:56 +0300

    added some new classes

--- src/master_deck.py
+++ src/master_deck.py
@@ -1,7 +1,7 @@
 from card import Card
+import random
 
-
-class Deck:
+class MasterDeck:
 
     def __init__(self):
 
@@ -11,22 +11,35 @@ class Deck:
         
         suits = ["hearts", "diamonds", "clubs", "spades"]
         numbers = [2,3,4,5,6,7,8,9,10,11,12,13,14]
-        
-
 
         for suit in suits:
             for number in numbers:
                 next_card = Card(suit, number)
                 self.deck.append(next_card)
 
-    def lay_deck(self):
-        
+        self.deck.append(Card("black-joker", 0))
+        self.deck.append(Card("red-joker", 0))
+
+    def scramble(self):
+
+        random.shuffle(self.deck)
+
+    def lay_out_deck(self):
+
         for card in self.deck:
             print(card.name)
-        
 
-deck = Deck()
+    def draw(self, number_of_cards: int, destination: list):
 
-deck.create_deck()
+        if number_of_cards >= len(self.deck):
+            number_of_cards = len(self.deck)
 
-deck.lay_deck()
+
+        for i in range(0, number_of_cards):
+            destination.append(self.deck.pop())
+
+
+d = MasterDeck()
+d.create_deck()
+d.lay_out_deck()
+print(len(d.deck))
