1f062695286f0126938bc6b992de97fcfc455273
alexander <ale.engelhardt@outlook.com>
2022-05-13 13:00:12 +0300

    update week7

--- src/entities/user.py
+++ src/entities/user.py
@@ -1,6 +1,16 @@
 class User:
+    """Class which keeps information about a users account.
 
+    Attributes:
+        username: String which corresponds with the username the user has chosen.
+        password: String which corresponds with the password the user has chosen.
+    """
     def __init__(self, username, password):
-
+        """The class Constructor. Creates a new user.
+        
+        Args:
+            username (str): String which corresponds with the username the user has chosen.
+            password (str): String which corresponds with the password the user has chosen.
+        """
         self.username = username
         self.password = password
